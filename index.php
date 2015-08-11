1e2bc20fc78c6272aeef59321c8e3e9305847520
Wesley Moraes <wesleycrz@hotmail.com>
2015-08-11 14:51:59 -0300

    Projeto - Sistema e Login e Cadastro
    Layout + Front + Back!
    sistema básico de login, cadastro e resetar a senha.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Final War Game!</title>
+    <link type="text/css" rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <div class="container">
+        
+    </div>
+</body>
+</html>
